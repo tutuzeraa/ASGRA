010e8d5485aefcb340a63fda56e4efca7e98d294
Artur Alves <arturacb@gmail.com>
2025-06-14 20:12:44 -0300

    loading data

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # SGSC - Scene Graph Scene Classification
 
 Using Scene Graphs and GAT's to classify scenes.
+
+
+## Instalation
+
+```bash
+micromamba create -n SGSC python=3.11 pytorch torchvision torchaudio -c pytorch -c nvidia
+micromamba activate SGSC
+pip install torch_geometric
+```
--- src/main.py
+++ src/main.py
@@ -1,6 +1,34 @@
+import os
+import json 
+import logging
+
+import torch
+from torch_geometric.loader import DataLoader
+from torch_geometric.data import Data, Dataset, Batch
+from torch_geometric.nn import GATv2Conv
+
+from datasets.places8 import Places8SceneGraphDataset
+
+
+logging.basicConfig(
+    level=logging.INFO,                           
+    format="%(asctime)s | %(levelname)s | %(name)s: %(message)s",
+    datefmt="%H:%M:%S",
+)
+logger = logging.getLogger(__name__)             
+
+data_dir = "./data/graphs"
 
 def main():
-    print("Hello world!")
+    # edge_index = torch.tensor([[0,1,1,2], [1,0,2,1]], dtype=torch.long)
+    # x = torch.tensor([[-1], [0], [1]], dtype=torch.float)
+    # data = Data(x=x, edge_index=edge_index)
+    # print(data)
+
+    dataset = Places8SceneGraphDataset(data_dir)
+    train_loader = DataLoader(dataset, batch_size = 1, shuffle=True)
+
+    print(next(iter(train_loader)))
 
 
 if __name__ == "__main__":
